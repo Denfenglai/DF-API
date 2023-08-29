9327217a5bbf4a457fcbbd2be1b8485476977f9e
DengFengLai <3139373986@qq.com>
2023-08-29 10:14:26 +0000

    Update

---
+++ Genshin/index.php
@@ -0,0 +1,17 @@
+<?php
+$imageDirectory = 'img/';
+$imageTypes = ['gif', 'jpg', 'png', 'jpeg', 'webp', 'bmp'];
+
+// 获取图片文件列表
+$images = glob($imageDirectory . '*.{'. implode(',', $imageTypes) . '}', GLOB_BRACE);
+
+if (count($images) === 0) {
+    die('没有找到图片文件。 MuaOoO ~ ' . $imageDirectory . ' 文件夹');
+}
+
+// 随机选择一张图片
+$randomImage = $images[array_rand($images)];
+
+// 输出图片
+header('Content-Type: image/' . pathinfo($randomImage, PATHINFO_EXTENSION));
+readfile($randomImage);
---
+++ a.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd  /www/wwwroot/api.dengfenglai.cloud
+git push -u origin main
